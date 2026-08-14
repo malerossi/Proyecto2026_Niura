a4ea784f5c234ea463ca4d12d37b02990925ca9d
malerossi <malena.agostina.rossi@gmail.com>
2026-08-14 14:35:14 -0300

    dale quie se puede

--- Niura/src/frontend/InicioSesionPaciente/inicioPaciente.tsx
+++ Niura/src/frontend/InicioSesionPaciente/inicioPaciente.tsx
@@ -1,6 +1,21 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 
 export default function IniciodeSesion (){
+
+    const navigate = useNavigate()
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault()
+        if (nombre !== "" && apellido !== "" && mail !== "" && dni !== "" && contraseña !== ""){
+            navigate ('/inicioPaciente')
+        }
+        else {
+            alert ('Porfavor, llenar sus datos.')
+        }
+    }
+
     const [nombre, setNombre] = useState<string>("");
     const [apellido, setApellido] = useState<string>("");
     const [mail, setMail] = useState<string>("");
@@ -8,12 +23,14 @@ export default function IniciodeSesion (){
     const [contraseña, setContraseña] = useState<string>("");
 
     return (
-        <div className="Datos">
+        <form className="Datos" onSubmit={handleSubmit}>
         <input type="text" required placeholder="Nombre" id="nombrePaciente" className="nombrePaciente" value= {nombre} onChange= {(e) => setNombre(e.target.value)} />
         <input type="text" required placeholder="Apellido" id="apellidoPaciente" className="apellidoPaciente" value= {apellido} onChange= {(e) => setApellido(e.target.value)} />
         <input type="text" required placeholder="Mail" id="mailPaciente" className="mailPaciente" value= {mail} onChange= {(e) => setMail(e.target.value)} />
         <input type="text" required placeholder="DNI" id="dniPaciente" className="dniPaciente" value= {dni} onChange= {(e) => setDni(e.target.value)} />
         <input type="password" required placeholder="Contraseña" id="contraseñaPaciente" className="contraseñaPaciente" value= {contraseña} onChange= {(e) => setContraseña(e.target.value)} />
-    </div>
+        <button type="submit" id='Enviar' className='Enviar'> Enviar </button>
+        </form>
     )
+
 }
