93a1edc84c3a33cabe631e349734790577f699e6
Your Name <you@example.com>
2026-08-14 14:33:53 -0300

    racha bastante hecha teniendo en cuenta que es con datos que me saco del culo

--- Niura/src/racha.tsx
+++ Niura/src/racha.tsx
@@ -4,16 +4,27 @@ interface DiaRacha {
     fecha: string;       // ej: "2026-08-14"
     completado: boolean; // true si cumplió la meta, false si no ❌/✅
   }
-  
+
   // 2. Simulación de los datos que vendrían del backend 📦
   const historialRacha: DiaRacha[] = [
     { fecha: "2026-08-10", completado: true },
-    { fecha: "2026-08-11", completado: true },
+    { fecha: "2026-08-11", completado: false },
     { fecha: "2026-08-12", completado: true },
     { fecha: "2026-08-13", completado: false },
     { fecha: "2026-08-14", completado: true },
   ];
   export const ListaRacha = () => {
+    let rachaActual = 0;
+    for (let i =0  ; i <= historialRacha.length; i++) {
+      if (historialRacha[i].completado === true) {
+        rachaActual++;
+        console.log(rachaActual)
+      } else {
+  rachaActual=0
+        break; // 🛑 La racha se rompe con el primer false
+      }
+    }
+    
     return (
       <div className="contenedor-racha">
         {historialRacha.map((dia, index) => (
@@ -27,3 +38,9 @@ interface DiaRacha {
       </div>
     );
   };
+
+
+
+// Recorremos desde el más reciente (último índice) hacia el más antiguo (índice 0)
+
+
