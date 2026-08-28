01e5d652c4f4076d2b35ca3b4e5ffce8059ddce3
MartoMorales <martin.morales.larrozag@gmail.com>
2026-08-28 09:03:06 -0300

    prisma models y funcion de la racha

--- backend/src/schemas/users.schemas.js
+++ backend/src/schemas/users.schemas.js
@@ -6,8 +6,6 @@ const patientUser = z.object({
     email: z.string().email(),
     dni: z.string().min(8).max(8).transform(Number),
     password: z.string().min(7, "La contraseña debe tener al menos 7 caracteres"), 
-    points: z.number().int(),   
-    companion: z.number().int()
 })
 
 const companionUser = z.object({
@@ -24,7 +22,6 @@ const doctorUser = z.object({
     surname: z.string(),
     email: z.email(),
     dni: z.string().min(8).max(8).transform(Number),
-    patientID: z.number().int(  ),
     password: z.string().min(7, "La contraseña debe tener al menos 7 caracteres"),
     tuition: z.number().int()
 })
