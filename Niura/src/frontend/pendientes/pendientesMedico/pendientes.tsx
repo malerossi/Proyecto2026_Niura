93b097f5c406e1cd6bf63cc697e16e54df0a6134
malerossi <malena.agostina.rossi@gmail.com>
2026-09-07 10:37:59 -0300

    ñeeeeeee

--- Niura/src/frontend/pendientes/pendientesMedico/pendientes.tsx
+++ Niura/src/frontend/pendientes/pendientesMedico/pendientes.tsx
@@ -10,58 +10,63 @@ type Tarea = {
 
 export default function Pendientes () {
 
-    const [tareas, setTareas] = useState<Tarea[]>([]);
-    const navigate = useNavigate();
+  type filter = 'all' | 'motriz' | 'cognitiva';
 
-    useEffect(() => {
-        fetch ("/api/mis-tareas martiiiiinn")
-        .then((res) => res.json())
-        .then((data: Tarea[]) => setTareas(data))
-        .catch((err) => console.error("Error cargando tareas:", err));
-    }, [])
+  const [tareas, setTareas] = useState<Tarea[]>([]);
+  const navigate = useNavigate();
 
-    const tareaMotriz = tareas.find((t) => t.tipo === "motriz");
-    const tareaCognitiva = tareas.find((t) => t.tipo === "cognitiva");
+  useEffect(() => {
+    fetch ("/api/mis-tareas martiiiiinn")
+    .then((res) => res.json())
+    .then((data: Tarea[]) => setTareas(data))
+    .catch(() => console.error("Sucedió un error con la conexión de la API. Verificar conexión."));
+  }, [])
 
-    const todoCompletado = tareas.length > 0 && tareas.every((t) => t.completada);
+  const tareaMotriz = tareas.find((t) => t.tipo === "motriz");
+  const tareaCognitiva = tareas.find((t) => t.tipo === "cognitiva");
 
-    const irAEjercicio = (tarea: Tarea) => {
-      navigate(`/ejercicio/${tarea.tipo}/${tarea.id}`);
-    };
+  const renderMotriz = tareas.filter((t) => t.tipo === 'motriz');
+  const renderCognitiva = tareas.filter((t) => t.tipo === 'cognitiva')
 
-    const EjerciciosDiarios = () => {
-      navigate('/ejercicios')
-    }
+  const todoCompletado = tareas.length > 0 && tareas.every((t) => t.completada === true);
 
-    return (
-        <div className="Pendientes">
-          <div className="banner-motivacional" onClick={() => EjerciciosDiarios()}>
-            {todoCompletado ? (
-              <h2>🎉 ¡Felicitaciones! Completaste tus tareas de hoy.</h2>
-            ) : (
-              <h2>💪 ¡Hora de tu entrenamiento diario!</h2>
-            )}
-          </div>
+  const irAEjercicio = (tarea: Tarea) => {
+    navigate(`/ejercicio/${tarea.tipo}/${tarea.id}`);
+  };
+
+  const EjerciciosDiarios = () => {
+    navigate('/ejercicios')
+  }
+
+  return (
+    <div className="Pendientes">
+      <div className="PopMotivacional" onClick={() => EjerciciosDiarios()}>
+        {todoCompletado ? (
+        <h2>🎉 ¡Felicitaciones! Completaste tus tareas de hoy.</h2>
+        ) : (
+          <h2>💪 ¡Hora de tu entrenamiento diario!</h2>
+        )}
+      </div>
     
-          {tareaMotriz && !tareaMotriz.completada && (
-            <div
-              className="PendientesMotriz"
-              onClick={() => irAEjercicio(tareaMotriz)}
-            >
-              <span>🖐️ Tarea Motriz Pendiente: {tareaMotriz.titulo}</span>
-              <p>Toca para hacer el ejercicio</p>
-            </div>
-          )}
-    
-          {tareaCognitiva && !tareaCognitiva.completada && (
-            <div
-              className="PendientesCognitiva"
-              onClick={() => irAEjercicio(tareaCognitiva)}
-            >
-              <span>🧠 Tarea Cognitiva Pendiente: {tareaCognitiva.titulo}</span>
-              <p>Toca para hacer el ejercicio &rarr;</p>
-            </div>
-          )}
+      {tareaMotriz && !tareaMotriz.completada && (
+        <div
+          className="PendientesMotriz"
+          onClick={() => irAEjercicio(tareaMotriz)}
+        >
+          <span>🖐️ Tarea Motriz Pendiente: {tareaMotriz.titulo}</span>
+          <p>Toca para hacer el ejercicio</p>
         </div>
-    )
+      )}
+    
+      {tareaCognitiva && !tareaCognitiva.completada && (
+        <div
+          className="PendientesCognitiva"
+          onClick={() => irAEjercicio(tareaCognitiva)}
+        >
+          <span>🧠 Tarea Cognitiva Pendiente: {tareaCognitiva.titulo}</span>
+          <p>Toca para hacer el ejercicio &rarr;</p>
+        </div>
+      )}
+    </div>
+  )
 }
