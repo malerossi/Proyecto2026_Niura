808d9ddbf2f92541ea381e14cd1149797e8ad73c
malerossi <malena.agostina.rossi@gmail.com>
2026-09-09 14:16:29 -0300

    contexto cuidador e inicio mejorado con contexto.

---
+++ Niura/src/frontend/Contexts/contextoInicioCuidador.tsx
@@ -0,0 +1,30 @@
+import { useContext, createContext, type ReactNode, useState } from 'react';
+
+interface contextoICType {
+    nombre: string;
+    setNombre: (nombre:string) => void;
+}
+
+const ContextoIC = createContext <contextoICType|(null)>(null);
+
+interface ProviderProps {
+    children: ReactNode;
+}
+
+export const InicioCuidadorProvider = ({ children }: ProviderProps) => {
+    const [nombre, setNombre] = useState<string>('');
+
+    return (
+        <ContextoIC.Provider value={{ nombre, setNombre }}>
+          {children}
+        </ContextoIC.Provider>
+      );
+}
+
+export const useInicioCuidador = () => {
+  const contexto = useContext (ContextoIC);
+  if (!contexto) {
+    throw ('Error al cargar el contexto. Revisar su implementación.')
+  }
+  return contexto;
+}
