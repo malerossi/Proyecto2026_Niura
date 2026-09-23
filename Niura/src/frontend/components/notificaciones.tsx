ebb519c922763ee7fcc060cb054a6e8e8d1d3e16
malerossi <malena.agostina.rossi@gmail.com>
2026-09-23 14:28:39 -0300

    desoues sigo

--- Niura/src/frontend/components/notificaciones.tsx
+++ Niura/src/frontend/components/notificaciones.tsx
@@ -1,13 +1,26 @@
 import { useEffect, useState } from "react";
+import type { Tarea } from "../pendientes/pendientesMedico/pendientes";
 
 
 export default function Notificaciones() {
     const [tareas, setTareas] = useState<Tarea[]>([]);
     
     useEffect(() => {
-        fetch("/api/notificaciones")
-        .then((res) => res.json())
-        .then((data: Tarea[]) => setTareas(data))
-        .catch((err) => console.error("Error al obtener notificaciones:", err));
+        fetch("/api/mis-tareas")
+            .then((res) => res.json())
+            .then((data: Tarea[]) => setTareas(data))
+            .catch(() => console.error("Sucedió un error con la conexión de la API. Verificar conexión."));
     }, []);
+    return (
+        <div className="notificacion">
+            <h2>Notificaciones pendientes ({tareas.length})</h2>
+            {tareas.length === 0 ? (
+                <p>No tienes tareas pendientes</p>
+            ) : (
+                <p>{tareas.map((tarea) => (
+                    <p key={tarea.id}>{tarea.titulo} {tarea.completada?}</p>
+                ))}</p>
+            )}
+        </div>
+    );
 }
